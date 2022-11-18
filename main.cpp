9d44131b4006b550f236451f957e50f1074cf71d
Carson-Bay <cgbman02@gmail.com>
2022-11-17 21:41:21 -0500

    Open Window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+using namespace glm;
+
+GLFWwindow* window;
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    glewExperimental = true;
+    if(!glfwInit()) {
+        printf("Failed to initialize GLFW\n");
+        return -1;
+    }
+    
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(1024, 768, "Solar System", NULL, NULL);
+    if (window == NULL) {
+        printf("Failed to open window");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glewExperimental = true;
+    if(glfwInit() != GLEW_OK) {
+        printf("Failed to initialize GLFW\n");
+        return -1;
+    }
+
+    glfwSetInputMode(window, GLFW_STICKY_KEYS,GL_TRUE);
+
+    do {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    } while(glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
+
 }
